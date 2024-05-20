c82e95dedcdf3da1dd785928e809b737fb7d4f43
-m <lnxterry@gmail.com>
2024-05-20 17:54:55 +0800

    Add CValues

---
+++ CValues/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <int N>
+void print_number() {
+    cout << "Printing the number: " << N << endl;
+}
+
+int main() {
+    print_number<1>(); // Printing the number: 1
+    print_number<2>(); // Printing the number: 2
+    print_number<3>(); // Printing the number: 3
+
+    return 0;
+}
